df03a915a68161c7ac03798ac1dd63c03bb5a176
Zachary Sheffler <zsheffle@asu.edu>
2026-06-10 16:08:28 -0700

    Fix defect-8 Magic number changed to totalCopies Closes 3

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -103,7 +103,7 @@ public class Book {
      * Increments available copies when book is returned.
      */
     public void returnBook() {
-        if (availableCopies < 100) {
+        if (availableCopies < totalCopies) {
             availableCopies++;
         }
         this.available = (availableCopies > 0);
